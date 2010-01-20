4099b997bb570b4a738c4031d82be69babc942af
Lenno Nagel <lenno@nagel.ee>
2010-01-20 16:17:41 +0000

    converted core/config.py to YAML

--- core/config.py
+++ core/config.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ConfigParser
+import yaml
 
 class Config:
     def __init__(self):
-        self.parser = ConfigParser.ConfigParser()
+        self.data = {}
         
-    def load(self, configfiles):
-        self.parser.read(configfiles)
+    def load(self, configfile):
+        cf = file(configfile, 'r')
+        self.data = yaml.load(cf)
+        cf.close()
+        
+    def save(self, configfile):
+        cf = file(configfile, 'w')
+        yaml.dump(self.data, cf, default_flow_style=False)
+        cf.close()
     
-    def printall(self):
-        for section in self.parser.sections():
+    def print_all(self):
+        for section in self.data:
             print "Section:", section
-            for option in self.parser.options(section):
-                print " ", option, "=", self.parser.get(section, option)
+            if self.data[section]:
+                for option in self.data[section]:
+                    print " ", option, "=", self.data[section][option]
 
 
--- printconfig.py
+++ printconfig.py
@@ -5,8 +5,8 @@ from core.config import Config
 from core.policy import Policy
 
 config = Config()
-
 config.load('config/main.conf')
+config.print_all()
 
 gp = Policy()
 gp.load(['config/groups.conf'])
