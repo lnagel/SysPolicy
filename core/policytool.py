21f8b36426867222c2bb54296e49c5156a825b19
Lenno Nagel <lenno@nagel.ee>
2010-01-25 17:06:20 +0000

    added state loading/creating

--- core/policytool.py
+++ core/policytool.py
@@ -8,25 +8,33 @@ from modules import Module
 class PolicyTool:
     def __init__(self,  configfile):
         self.conf = Config(configfile)
-        self.pol = {}
-        self.handlers = {}
+        self.policy = {}
+        self.state = {}
+        self.handler = {}
         for type,  file in self.conf.get(['policy']).items():
-            self.pol[type] = Policy(file)
-        self.mods = {}
+            self.policy[type] = Policy(self.conf.get(['general', 'policy-path'])+'/'+file)
+            try:
+                self.state[type] = Policy(self.conf.get(['general', 'state-path'])+'/'+file)
+            except IOError:
+                self.state[type] = Policy(self.conf.get(['general', 'state-path'])+'/'+file,  False)
+                self.state[type].save()
+        self.module = {}
     
     def add_module(self,  module):
         if isinstance(module, Module):
             print module,  "is a module!"
-            self.mods[module.name] = module
+            self.module[module.name] = module
             for policy_type,  attributes in module.handled_attributes.items():
                 for attribute in attributes:
                     self.register_handler(policy_type,  attribute,  module)
     
     def register_handler(self,  policy_type,  attribute,  module):
-        if policy_type not in self.handlers:
-            self.handlers[policy_type] = {}
-        if attribute not in self.handlers[policy_type]:
-            self.handlers[policy_type][attribute] = module
+        if policy_type not in self.handler:
+            self.handler[policy_type] = {}
+        if attribute not in self.handler[policy_type]:
+            self.handler[policy_type][attribute] = module
             print "Registered handler: policy",  policy_type,  "attribute",  attribute,  "to",  module
         else:
             raise Exception("Handler has already been set for policy '" + policy_type + "' attribute '" + attribute + "'")
+
+
