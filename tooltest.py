adf35f0852cf18ee709c078ccac0fd98b1e332dc
Lenno Nagel <lenno@nagel.ee>
2010-02-25 15:23:59 +0000

    state saving test

--- tooltest.py
+++ tooltest.py
@@ -12,3 +12,9 @@ pt.add_module(Shadow())
 print yaml.dump(pt.get_policy_diff())
 
 pt.get_policy_updates()
+
+pt.set_state('groups', ['www-users', 'basedir'], '/srv')
+
+print yaml.dump(pt.state)
+pt.save_state()
+
