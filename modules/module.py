4114958e9366143998923600543ca27bd8528d46
Lenno Nagel <lenno@nagel.ee>
2010-03-01 13:12:27 +0000

    First bits of the diff interpreter. Refs #4.

--- modules/module.py
+++ modules/module.py
@@ -1,6 +1,22 @@
 
+import core.config as config
 
 class Module:
     def __init__(self):
         self.name = "generic"
         self.handled_attributes = {}
+
+    def check_diff(self, policy,  state, path, value):
+        print "check_diff for",  path, ",", value
+        if len(path) >= 2:
+            group = path[0]
+            attribute = path[1]
+            operation = config.diff_type(policy,  state,  path)
+            
+            if group == config.DEFAULT:
+                print "default setting:", attribute, "=", value
+            else:
+                print "group setting", attribute, "=", value
+            
+        
+        return None
