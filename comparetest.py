ec3693d0c4a923af4d4e1e37e89ae16c33260d96
Lenno Nagel <lenno@nagel.ee>
2010-03-01 13:13:50 +0000

    simple test for the compare_trees method

---
+++ comparetest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from core.config import compare_trees
+import yaml
+
+a = {'aaa': 'hehehe',  'bbb': 'asdf',  'tree': {'asd': 1, 'kekekk': 42},  'list': ['a'], 'empty': None}
+b = {'bbb': 'asd' ,  'tree': {'asd': 1}, 'list': ['a']}
+
+r = compare_trees(a, b)
+print yaml.dump(r, default_flow_style=False)
