9ca723788dbc60b7ec24e6d24dc351c4c72b459c
Lenno Nagel <lenno@nagel.ee>
2010-03-08 15:58:38 +0000

    Turning Worker into a daemon thread. Refs #9

--- core/worker.py
+++ core/worker.py
@@ -6,10 +6,12 @@ from Queue import Queue
 class Worker(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        self.daemon = True
         self.queue = Queue()
         self.log = Queue()
     
     def run(self):
+        print "== Worker started =="
         while True:
             cs = self.queue.get()
             print "Worker processing ChangeSet", cs
