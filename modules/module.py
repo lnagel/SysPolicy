8ec24dc2e77121ba0254d8fb096abfb41db52af6
Lenno Nagel <lenno@nagel.ee>
2010-03-11 16:18:49 +0000

    Appending a ChangeSet for updating the state in a new state module. Refs #13

--- modules/module.py
+++ modules/module.py
@@ -1,11 +1,13 @@
 
 import core.config as config
 import core.change
+from core.change import Change, ChangeSet
 
 class Module:
     def __init__(self):
         self.name = "generic"
         self.handled_attributes = {}
+        self.pt = None
         self.diff_operations = {
                             config.CONFIG_ADDED: self.pol_new_attribute,
                             config.CONFIG_CHANGED: self.pol_set_attribute,
@@ -15,17 +17,20 @@ class Module:
 
     def pol_check_diff(self, policy,  state, path, value):
         print "pol_check_diff for",  path, ",", value
+        cs = None
         if len(path) >= 2:
             group = path[0]
             attribute = path[1]
             operation = config.diff_type(policy,  state,  path)
-            cs = None
             
             if group == config.DEFAULT:
                 print "assign default setting:", attribute, "=", value
                 cs = self.pol_set_default(attribute, value)
             elif operation in self.diff_operations:
                 cs = self.diff_operations[operation](group, attribute, value)
+        
+        if cs is not None:
+            cs.append(Change("state", "set_state", {"path": path, "value": value, "diff_type": operation}))
         
         return cs
 
