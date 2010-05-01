0e98301a755b8a7cc2942aab3e9651ec8a887ef9
Lenno Nagel <lenno@nagel.ee>
2010-05-01 13:01:44 +0000

    Refactor the contents of argtest.py into syspolicy.cli.interface function main(); Refs #16

--- argtest.py
+++ argtest.py
@@ -1,51 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from syspolicy.policytool import PolicyTool
-from syspolicy.modules.shadow import Shadow
-from syspolicy.modules.pam import PAM
-from syspolicy.modules.state import State
-from syspolicy.cli.prompt import confirm
-import yaml
-from optparse import OptionParser
-
-parser = OptionParser()
-
-parser.add_option("-c", "--config", 
-                  action="store", dest="config", default="config/main.conf", 
-                  help="load configuration from FILE", metavar="FILE")
-parser.add_option("-u", "--update",
-                  action="store_true", dest="mode_update", default=False,
-                  help="check for policy updates")
-parser.add_option("-s", "--scan",
-                  action="store_true", dest="mode_scan", default=False,
-                  help="perform a full system scan to verify the policy")
-
-(options, args) = parser.parse_args()
-
-if options.mode_update:
-    print "update mode!"
-    
-    pt = PolicyTool(options.config)
-    pt.add_module(Shadow())
-    pt.add_module(PAM())
-    pt.add_module(State())
-    pt.get_policy_updates()
-    
-    print "------- ChangeSets -------"
-    with pt.cs_mlock:
-        for cs in pt.changesets:
-            print yaml.dump(cs)
-            if confirm("Approve this ChangeSet?"):
-                pt.accept_changeset(cs)
-    
-    if len(pt.changesets) > 0 and confirm("Enqueue %d ChangeSets?" % len(pt.changesets)):
-        pt.enqueue_changesets()
-    else:
-        print "Nothing to do.."
-    
-    pt.worker.queue.join()
-    pt.save_state()
-else:
-    parser.print_help()
-
+from syspolicy.cli.interface import main
+main()
