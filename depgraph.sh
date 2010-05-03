03b092ae0a1b3d65d164ce489ae024fd976a127b
Lenno Nagel <lenno@nagel.ee>
2010-05-03 15:53:33 +0000

    Removing tooltest.sh since it's getting deprecated now - equivalent testing and more is already provided by cli.sh

--- depgraph.sh
+++ depgraph.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sfood tooltest.py argtest.py syspolicy/ | sfood-graph | dot -Tps2 | ps2pdf14 - depgraph.pdf
+sfood cli.py syspolicy/ | sfood-graph | dot -Tps2 | ps2pdf14 - depgraph.pdf
