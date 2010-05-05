e54c20ea496cded3c375170f9b6ed9ba31c97d23
Lenno Nagel <lenno@nagel.ee>
2010-05-05 13:37:27 +0000

    Added the list of arguments as a comment from the Wiki, starting to implement them in groups. Refs #16

--- syspolicy/cli/arguments.py
+++ syspolicy/cli/arguments.py
@@ -1,18 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 
 class OptParser(OptionParser):
     def __init__(self):
         OptionParser.__init__(self)
+        
+        # = Command line arguments =
+        #* -h, --help
+        #* -c, --config=FILE
+        #* -u, --update
+        #* -s, --scan
+        #* --au, --add-user=USER
+        #* --mu, --mod-user=USER
+        #** -p, --password
+        #** -n, --name=REALNAME
+        #** -g, --group=GROUP
+        #** --homedir/--no-homedir
+        #* --du, --del-user=USER
+        #* --ag, --add-group=GROUP
+        #* --dg, --del-group=GROUP
+        
+        self.set_defaults(config='config/main.conf')
+        self.set_defaults(mode_update=False)
+        self.set_defaults(mode_scan=False)
 
-        self.add_option("-c", "--config", 
-              action="store", dest="config", default="config/main.conf", 
-              help="load configuration from FILE", metavar="FILE")
-        self.add_option("-u", "--update",
-              action="store_true", dest="mode_update", default=False,
-              help="check for policy updates")
-        self.add_option("-s", "--scan",
-              action="store_true", dest="mode_scan", default=False,
-              help="perform a full system scan to verify the policy")
+        self.add_option("-c", "--config",
+                dest="config", metavar="FILE", 
+                help="load configuration from FILE")
+      
+        mode = OptionGroup(self, "Run modes")
+        mode.add_option("-u", "--update",
+                action="store_true", dest="mode_update",
+                help="check for policy updates")
+        mode.add_option("-s", "--scan",
+                action="store_true", dest="mode_scan",
+                help="perform a full system scan to verify the policy")
+        self.add_option_group(mode)
+        
+        user = OptionGroup(self, "User management")
+        user.add_option("--au", "--add-user",
+                dest="add_user", metavar="USER", 
+                help="Add a new user account")
+        user.add_option("--mu", "--mod-user",
+                dest="mod_user", metavar="USER", 
+                help="Modify a user account")
+        self.add_option_group(user)
+
