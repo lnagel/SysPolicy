3c1854c6298680dae86a6129ceb3c9e9a66d4603
Lenno Nagel <lenno@nagel.ee>
2010-05-06 08:23:56 +0000

    Creating a module/class for the Quota module

--- syspolicy/modules/autoloader.py
+++ syspolicy/modules/autoloader.py
@@ -1,8 +1,10 @@
 from syspolicy.modules.shadow import Shadow
 from syspolicy.modules.pam import PAM
+from syspolicy.modules.quota import Quota
 from syspolicy.modules.state import State
 
 def autoload_modules(pt):
     pt.add_module(Shadow())
     pt.add_module(PAM())
+    pt.add_module(Quota())
     pt.add_module(State())
---
+++ syspolicy/modules/quota.py
@@ -0,0 +1,22 @@
+
+import syspolicy.change
+from syspolicy.change import Change, ChangeSet
+from syspolicy.modules.module import Module
+
+class Quota(Module):
+    def __init__(self):
+        Module.__init__(self)
+        self.name = "quota"
+        self.handled_attributes['groups'] = ['userquota', 'groupquota']
+        self.change_operations['set_default'] = self.set_default
+    
+    def pol_set_default(self, attribute,  value):
+        print "Setting new default in the Quota module", attribute, "=", value
+        return ChangeSet(Change("quota", "set_default", {attribute: value}))
+    
+    def pol_set_attribute(self, group, attribute, value):
+        print "Setting attribute value in the Quota module", attribute, "=", value
+        return ChangeSet(Change(self.name, "set_attribute", {'group': group, 'attribute': attribute, 'value': value}))
+
+    def set_default(self, change):
+        return syspolicy.change.STATE_COMPLETED
