dad5e77aa96100b4f2134a9a810dffea7eda76dc
Lenno Nagel <lenno@nagel.ee>
2010-05-08 09:28:02 +0000

    Restructured the CLI interface module in preparations for adding other run modes. Refs #16

--- syspolicy/cli/interface.py
+++ syspolicy/cli/interface.py
@@ -10,27 +10,32 @@ from syspolicy.cli.arguments import OptParser
 
 def main():
     parser = OptParser()
-    (options, args) = parser.parse_args()
+    (opts, args) = parser.parse_args()
+    
+    o = [opts.mode_update, opts.mode_scan, opts.add_user, 
+              opts.mod_user, opts.del_user, opts.add_group, opts.del_group]
+    
+    # How many values that we have that aren't None or False?
+    if len(o) - o.count(None) - o.count(False) != 1:
+        parser.error("You have to specify exactly 1 run mode option")
+    
+    pt = PolicyTool(opts.config)
 
-    if options.mode_update:
+    if opts.mode_update:
         print "update mode!"
-        
-        pt = PolicyTool(options.config)
         pt.get_policy_updates()
-        
-        print "------- ChangeSets -------"
-        with pt.cs_mlock:
-            for cs in pt.changesets:
-                print yaml.dump(cs)
-                pt.accept_changeset(cs, confirm("Approve this ChangeSet?"))
-        
-        if len(pt.changesets) > 0 and confirm("Enqueue %d ChangeSets?" % len(pt.changesets)):
-            pt.enqueue_changesets()
-        else:
-            print "Nothing to do.."
-        
-        pt.worker.queue.join()
-        pt.save_state()
+    
+    print "------- ChangeSets -------"
+    with pt.cs_mlock:
+        for cs in pt.changesets:
+            print yaml.dump(cs)
+            pt.accept_changeset(cs, confirm("Approve this ChangeSet?"))
+    
+    if len(pt.changesets) > 0 and confirm("Enqueue %d ChangeSets?" % len(pt.changesets)):
+        pt.enqueue_changesets()
     else:
-        parser.print_help()
+        print "Nothing to do.."
+    
+    pt.worker.queue.join()
+    pt.save_state()
 
