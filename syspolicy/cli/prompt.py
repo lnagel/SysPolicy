a70d946367be84c37c9f025c3091b98a3e488f69
Lenno Nagel <lenno@nagel.ee>
2010-05-24 19:01:25 +0000

    Implemented a cracklib-based password check mechanism that takes parameters from the services policy. Closes #33.

--- syspolicy/cli/prompt.py
+++ syspolicy/cli/prompt.py
@@ -3,6 +3,8 @@ import string
 from random import choice
 import crypt
 import getpass
+import string
+from _cracklib import FascistCheck
 
 SALT_TYPE = 6
 SALT_SIZE = 16
@@ -45,6 +47,46 @@ def genpass(size=PASSWORD_SIZE):
     pool = string.letters + string.digits
     return ''.join([choice(pool) for i in range(size)])
 
+def checkpass(password, policy={}):
+    minlen = policy.get('minlen', PASSWORD_SIZE)
+    minclass = policy.get('minclass', 0)
+    dcredit = policy.get('dcredit', 1)
+    ucredit = policy.get('ucredit', 1)
+    lcredit = policy.get('lcredit', 1)
+    ocredit = policy.get('ocredit', 1)
+    
+    dcount = 0
+    ucount = 0
+    lcount = 0
+    ocount = 0
+    ccount = 0
+    bonus = 0
+    
+    for c in password:
+        if c in string.digits:
+            dcount += 1
+        elif c in string.ascii_lowercase:
+            lcount += 1
+        elif c in string.ascii_uppercase:
+            ucount += 1
+        else:
+            ocount += 1
+    
+    for (credit, count) in [(dcredit, dcount), (ucredit, ucount),
+                            (lcredit, lcount), (ocredit, ocount)]:
+        # count char classes that have been seen at least once
+        ccount += min(1, count)
+        # add up to credit or count amount of bonus points per class
+        bonus += min(credit, count)
+    
+    if ccount < minclass:
+        raise ValueError('not enough different character classes')
+    
+    if (len(password) + bonus) < minlen:
+        raise ValueError('it is too short')
+
+    return FascistCheck(password)
+
 def setpwd(policy={}):
     size = policy.get('minlen', PASSWORD_SIZE)
     size -= policy.get('minclass', 0)
@@ -56,7 +98,7 @@ def setpwd(policy={}):
     print policy
     input = getpass.getpass("Accept or enter another: ")
     password = crypt.crypt(input, salt)
-    if len(input) >= size:
+    if checkpass(input, policy):
         if input == getpass.getpass("Repeat password: "):
             return password
 
