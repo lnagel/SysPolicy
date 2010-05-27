93570bc17ddda18659d10a6ee733e98f2ed621f3
Lenno Nagel <lenno@nagel.ee>
2010-05-27 19:56:52 +0000

    Documented the State module

--- syspolicy/modules/state.py
+++ syspolicy/modules/state.py
@@ -15,18 +15,34 @@ from syspolicy.change import Change, ChangeSet
 from syspolicy.modules.module import Module
 
 class State(Module):
+    """
+    This module provides internal policy state management for SysPolicy
+    """
+    
     def __init__(self):
         Module.__init__(self)
         self.name = "state"
         self.change_operations['set_state'] = self.set_state
     
     def cs_check_diff(self, policy, operation, path, value, diff):
+        """
+        This function produces a ChangeSet based on the changed
+        policy attribute. The Change element will be executed
+        by the set_state function.
+        """
         state_update = Change(self.name, "set_state", 
                              {"policy": policy, "path": path,
                                 "value": value, "diff_type": operation})
         return ChangeSet(state_update)
     
     def set_state(self, change):
+        """
+        This function implements a set_state Change operation,
+        simply storing the new value in the policy state file.
+        
+        @param change: Change element
+        @return: STATE_COMPLETED or STATE_FAILED
+        """
         p = change.parameters
         policy = p['policy']
         path = p['path']
